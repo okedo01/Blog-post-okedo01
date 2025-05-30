1a7c2c96467e40a5fa16ad23be7fdc61c183f78f
Okedo01 <eliahmwelangi01@gmail.com>
2025-05-30 10:42:18 +0300

    Install react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { Link, Route, Routes } from "react-router-dom"
+import Home from "./Pages/NavBar/Home"
+import About from "./Pages/NavBar/About"
+import Contact from "./Pages/NavBar/Contact"
+import "./App.css"
+
 function App() {
 
   return (
     <>
-      <h1>Blog Post App</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   )
 }
