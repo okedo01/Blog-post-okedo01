3a7c6d173eeb9878e66016ebefcbe7ea9cc9a3a5
Okedo01 <eliahmwelangi01@gmail.com>
2025-05-31 10:00:21 +0300

    Create Add Post component to allow user to add new post

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ import PostDetails from "./Pages/PostDetails";
 import Layout from "./Pages/Layout";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import { useState } from "react";
+import AddPosts from "./Pages/AddPosts";
+import type { Post } from "./Type";
 
 function App() {
+  const [ post, setPost ] = useState<Post[]>(initialPosts)
+
+  const handleAddPost = (post: Post) => {
+    setPost((prevPosts) => [...prevPosts, post])
+  }
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -16,6 +24,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="post" element={<BlogList />} />
         <Route path="post/:id" element={<PostDetails />} />
+        <Route path="add" element={<AddPosts onAddPost={handleAddPost}/>} />
       </Route>
     </Routes>
   );
